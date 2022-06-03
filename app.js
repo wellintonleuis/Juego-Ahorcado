1e7503273376a6674f9935e2f6de55e352268ccb
luis <lemm_02@hotmail.com>
2022-06-03 14:09:42 -0600

    codificar funciones de trancision

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 var juego = {
     palabra: "ALURA",
-    estado: 1,
+    estado: 7,
     adivinado: ["A", "L"],
     errado: ["B", "J", "K", "C"]
 }
@@ -17,11 +17,18 @@ var $html = {
 function dibujar(juego) {
     var $elem
     $elem = $html.hombre
-    $elem.src = "./img/estados/0" + juego.estado + ".png"
+
+    var estado = juego.estado
+    if (estado == 8){
+        estado = juego.previo
+    }
+    $elem.src = "./img/estados/0" + estado + ".png"
     
     var palabra = juego.palabra
     var adivinado = juego.adivinado
     $elem = $html.adivinado
+
+    $elem.innerHTML = ""
     for(let letra of palabra) {
        let $span = document.createElement("span")
        let $txt = document.createTextNode("")
@@ -35,6 +42,8 @@ function dibujar(juego) {
 
     var errado = juego.errado
     $elem = $html.errado
+
+    $elem.innerHTML = ""
     for (let letra of errado) {
         let $span = document.createElement("span")
         let $txt = document.createTextNode(letra)
@@ -45,8 +54,50 @@ function dibujar(juego) {
     }
 }
 
-console.log(juego)
-console.log(dibujar(juego))
+function adivinar(juego, letra){
+    var estado = juego.estado
+    if (estado == 1 || estado == 8){
+        return
+    }
+    var adivinado = juego.adivinado
+    var errado = juego.errado
+    if(adivinado.indexOf(letra) >= 0 || errado.indexOf(letra) >= 0){
+        return
+    }
+    var palabra = juego.palabra
+    if(palabra.indexOf(letra) >= 0){
+        let ganado = true 
+        for (let l of palabra){
+            if (adivinado.indexOf(l) < 0 && l != letra){
+                ganado = false
+                juego.previo = juego.estado 
+                break
+                
+            }
+
+        }
+        if(ganado){
+            juego.estado = 8
+        }
+        adivinado.push(letra)
+    }else{
+        juego.estado--
+        errado.push(letra)
+    }
+}
+
+window.onkeypress = function adivinarLetra(e){
+    var letra = e.key
+    letra = letra.toUpperCase()
+    if (/[^A-ZÑ]/.test(letra)){
+        return
+    }
+    adivinar(juego, letra)
+    dibujar(juego)
+
+}
+
+dibujar(juego)
 
 
 
